296fe1cfb800233ca967e6a8f7bf52135f6854d6
Claudia Rocule <csaaas@hotmail.com>
2025-04-25 09:42:49 +0200

    Cambiando ubicacion de mapa

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event)=> {
     setTimeout(() => {
     document.querySelector("#load-iframe-map").innerHTML = `
-    <iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d801.3679688392019!2d-4.646314224104092!3d36.542733693809744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72e135ca7900d1%3A0x3ca5ecc7fac721a8!2sC.%20R%C3%ADo%20Guadalteba%2C%2018%2C%2029651%20Las%20Lagunas%20de%20Mijas%2C%20M%C3%A1laga!5e0!3m2!1ses!2ses!4v1744024868844!5m2!1ses!2ses"></iframe>
+    <iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51170.895407404576!2d-4.490466339963751!3d36.71821490105197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7259c44fdb212d%3A0x6025dc92c9ca32cf!2zTcOhbGFnYQ!5e0!3m2!1ses!2ses!4v1745566331275!5m2!1ses!2ses"></iframe>
+
     `;
     }, 500);
 });
